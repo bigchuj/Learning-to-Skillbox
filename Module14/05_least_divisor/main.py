faadcade68c8b7ba4075995ba2ce4c3ef3d4e403
sergei_chuev_1 <bigchuj1@gmail.com>
2022-07-26 01:02:50 +0300

    The homework is completed

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,14 @@
 # TODO здесь писать код
+
+def min_divider(num):
+
+    for i in range(2, num + 1):
+
+        if not num % i:
+            print("Наименьший делитель, отличный от единицы:", i)
+            break
+
+
+
+num = int(input("Введите число: "))
+min_divider(num)
--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,22 @@
 # TODO здесь писать код
+
+
+def search_coin(
+    first_coordinate, second_coordinate, radius
+):
+
+    if (
+        pow(first_coordinate, 2) + pow(second_coordinate, 2) <= pow(radius, 2)
+    ):
+        print('\nМонетка где-то рядом')
+
+    else:
+        print('\nМонетки в области нет')
+
+
+print("Введите координаты монетки:")
+first_coordinate = float(input('X: '))
+second_coordinate = float(input('Y: '))
+radius = float(input('Введите радиус: '))
+
+search_coin(first_coordinate, second_coordinate, radius)
