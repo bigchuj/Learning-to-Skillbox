2ec4236187a990c7513a507341c4af017b2d1bc0
sergei_chuev_1 <bigchuj1@gmail.com>
2022-11-07 01:39:52 +0300

    скорректирован код задания 5

--- Module24/05_happy_farm_2/main.py
+++ Module24/05_happy_farm_2/main.py
@@ -23,31 +23,92 @@ class Potato:
 
 
 class PotatoGarden:
-    def __init__(self, count):
-        self.potatoes = [Potato(index) for index in range(1, count + 1)]
+    def __init__(self, count_potato, count_potato_bed):
+        # сделали количество грядок и количетсво картофеля на грядке
+        self.potatoes = {
+            key: [
+                Potato(index) for index in range(1, count_potato + 1)
+            ] for key in range(1, count_potato_bed + 1)
+        }
 
-    def grow_all(self):
-        print('Картошка прорастает!')
-        for potato in self.potatoes:
-            potato.grow()
+    def grow_all(self, num_potato_bed):
+        # передаем агрумент - номер грядки, за которой будет ухаживать садовник
+        try:
+            if self.potatoes[num_potato_bed]:
+                for potato in self.potatoes[num_potato_bed]:
+                    potato.grow()
+                print('Картошка прорастает!')
+                return True
+            else:
+                return self.are_all_ripe(num_potato_bed)
 
-    def are_all_ripe(self):
-        for potato in self.potatoes:
-            if not potato.is_ripe():
-                print('Картошка ещё не созрела!\n')
-                return False  # такой return поможет получить информацию о зрелости картошки снаружи этого объекта
-        else:
-            print('Вся картошка созрела. Можно собирать!\n')
-            return True
+        except KeyError:
+            error_num_garden_bed()
+
+    def are_all_ripe(self, num_potato_bed_for_control):
+        try:
+            if not self.potatoes[num_potato_bed_for_control]:
+
+                print("Грядка пустая, посадите на ней картошку.")
+                option = input(
+                    'Посадить картошку? "да" - любой символ, "нет" - нажать "Enter": ')
+
+                if option:
+                    self.potatoes[num_potato_bed_for_control] = [
+                        Potato(index) for index in range(1, count_potato + 1)
+                    ]
+                else:
+                    return False
+
+            for potato in self.potatoes[num_potato_bed_for_control]:
+                if not potato.is_ripe():
+                    print('Картошка ещё не созрела!\n')
+                    return False 
+            else:
+                print('Вся картошка созрела. Можно собирать!\n')
+                return True
+
+        except KeyError:
+            error_num_garden_bed()
+
+    def collect_ripe(self, num_potato_bed):
+        print(f"Картошка с грядки {num_potato_bed} собрана, грядка пустая.")
+        self.potatoes[num_potato_bed].clear()
 
     def print_all_states(self):
         for potato in self.potatoes:
             potato.print_state()
 
 
-garden = PotatoGarden(5)
+def error_num_garden_bed():
 
-for _ in range(3):
-    garden.grow_all()
+    print("Вы ввели неверный номер грядки, такого количества грядок нет.")
+    option_2 = input(
+        'Хотите посадить еще грядку? "да" - любой символ, "нет" - нажать "Enter": ')
 
-garden.are_all_ripe()
+    if option_2:
+        num_potato_bed_new = len(garden.potatoes) + 1
+        garden.potatoes[num_potato_bed_new] = [
+            Potato(index) for index in range(1, count_potato + 1)
+        ]
+
+
+count_potato = int(input("Введите количество картошки на грядках: "))
+count_potato_bed = int(input("Введите количество грядок: "))
+garden = PotatoGarden(count_potato, count_potato_bed)
+
+
+while True:
+
+    num_potato_bed = int(
+        input("Введите номер грядки за которой требуется уход: "))
+
+    if garden.grow_all(num_potato_bed):
+        num_potato_bed_for_control = int(
+            input("Введите номер грядки, которую требуется проверить: "))
+
+        if garden.are_all_ripe(num_potato_bed_for_control):
+            option = input(
+                'Собрать картофель? "да" - любой символ, "нет" - нажать "Enter": ')
+            if option:
+                garden.collect_ripe(num_potato_bed_for_control)
